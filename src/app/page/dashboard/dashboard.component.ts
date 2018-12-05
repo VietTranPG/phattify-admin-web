ffa7731f1f31b110543d5e1b749d895207ad1b18
thaitrandinhpg <thai.trandinh@powergatesoftware.com>
2018-12-05 11:23:21 +0700

    Style for coming soon and select box

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -13,7 +13,7 @@ import './../../../assets/charts/amchart/usaLow.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   public summmury = {
