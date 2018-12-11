9e171195daa14398720bab7f07acff0311ee2c22
thaitrandinhpg <thai.trandinh@powergatesoftware.com>
2018-12-11 16:37:49 +0700

    Css for Client Info

--- src/app/page/client-info/client-info.component.ts
+++ src/app/page/client-info/client-info.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-client-info',
   templateUrl: './client-info.component.html',
-  styleUrls: ['./client-info.component.css']
+  styleUrls: ['./client-info.component.scss']
 })
 export class ClientInfoComponent implements OnInit {
 
