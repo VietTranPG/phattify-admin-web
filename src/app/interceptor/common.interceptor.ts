729ca9e3ed596c3986a77d54b9855f42b2544e6e
unknown <viet.tranhoang@powergatesoftware.com>
2018-12-13 13:59:13 +0700

    auth headers

--- src/app/interceptor/common.interceptor.ts
+++ src/app/interceptor/common.interceptor.ts
@@ -4,8 +4,15 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class CommonHttpInterceptor implements HttpInterceptor {
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let headers: any;
+    if (localStorage.getItem('userInfo') && JSON.parse(localStorage.getItem('userInfo')).data) {
+      headers = new HttpHeaders().set('Authorization', `Bearer ${JSON.parse(localStorage.getItem('userInfo')).data}`);
+    }
     // const headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return next.handle(req.clone());
+    return next.handle(req.clone({
+      headers: headers,
+    }));
   }
 }
