4a2a1797b4780f9951f1f51a9ff1ddb94cd37996
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2018-12-25 18:00:31 +0700

    change text

--- src/app/constants/config.ts
+++ src/app/constants/config.ts
@@ -1,8 +1,6 @@
 // UAT
 // export const SERVER_URL = "http://ilosemobileapp.azurewebsites.net/api/";
 
-// DEV
-// export const SERVER_URL = "http://admin-phattify-qa.azurewebsites.net/api";
 export const SERVER_URL = "http://phattifymobileservice.azurewebsites.net/api/";
 // export const SERVER_URL = "http://localhost:3000/api/";
 export const STATUS = {
