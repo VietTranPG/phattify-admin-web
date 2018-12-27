93804310705e4be7132e2d0e8403cbe4a3994dcf
thaitrandinhpg <thai.trandinh@powergatesoftware.com>
2018-12-27 10:33:02 +0700

    Style for Coach management page

--- src/app/page/coach-management/coach-management.component.ts
+++ src/app/page/coach-management/coach-management.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-coach-management',
   templateUrl: './coach-management.component.html',
-  styleUrls: ['./coach-management.component.css']
+  styleUrls: ['./coach-management.component.scss']
 })
 export class CoachManagementComponent implements OnInit {
 
