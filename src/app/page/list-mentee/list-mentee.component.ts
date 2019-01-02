22a70ca622a0c0c145c0144074b743332078a3b5
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2019-01-02 15:55:13 +0700

    list client

---
+++ src/app/page/list-mentee/list-mentee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-mentee',
+  templateUrl: './list-mentee.component.html',
+  styleUrls: ['./list-mentee.component.css']
+})
+export class ListMenteeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
