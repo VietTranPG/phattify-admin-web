a921243fde14cab8bffa33a0c7125d74dec97efc
unknown <viet.tranhoang@powergatesoftware.com>
2019-01-04 14:31:13 +0700

    email compose

---
+++ src/app/shared/email-compose/email-compose.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'email-compose',
+  templateUrl: './email-compose.component.html',
+  styleUrls: ['./email-compose.component.css']
+})
+export class EmailComposeComponent implements OnInit {
+  public editor;
+  public editorContent;
+  public editorConfig = {
+    placeholder: 'Put your things hear'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onEditorBlured(quill) {
+    console.log('editor blur!', quill);
+  }
+
+  onEditorFocused(quill) {
+    console.log('editor focus!', quill);
+  }
+
+  onEditorCreated(quill) {
+    this.editor = quill;
+    console.log('quill is ready! this is current quill instance object', quill);
+  }
+
+  onContentChanged({ quill, html, text }) {    
+    console.log('quill content is changed!', html);
+  }
+}
